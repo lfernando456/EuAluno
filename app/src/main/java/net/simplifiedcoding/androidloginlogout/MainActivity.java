0e0552785700c90db9cdad40cea8df35eb07d5a4
André <andrezoqui@gmail.com>
2016-04-01 10:45:01 -0300

    Correção

---
+++ app/src/main/java/net/simplifiedcoding/androidloginlogout/MainActivity.java
@@ -0,0 +1,104 @@
+package net.simplifiedcoding.androidloginlogout;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText editTextName;
+    private EditText editTextLastName;
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+    private EditText editTextEmail;
+
+    private Button buttonRegister;
+
+    private Button buttonLogin;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        editTextLastName = (EditText) findViewById(R.id.editTextLastName);
+        editTextUsername = (EditText) findViewById(R.id.editTextUserName);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+
+        buttonRegister = (Button) findViewById(R.id.buttonRegister);
+
+        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+
+        buttonRegister.setOnClickListener(this);
+
+        buttonLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == buttonRegister){
+            registerUser();
+        }
+        if(v == buttonLogin){
+            startActivity(new Intent(this,LoginActivity.class));
+        }
+    }
+
+    private void registerUser() {
+        final String name = editTextName.getText().toString().trim();
+        final String lastname = editTextLastName.getText().toString().trim();
+        final String username = editTextUsername.getText().toString().trim();
+        final String password = editTextPassword.getText().toString().trim();
+        final String email = editTextEmail.getText().toString().trim();
+
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, Config.REGISTER_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(MainActivity.this,response,Toast.LENGTH_LONG).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this,error.toString(),Toast.LENGTH_LONG).show();
+                    }
+                }){
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                params.put(Config.KEY_NAME,name);
+                params.put(Config.KEY_LASTNAME,lastname);
+                params.put(Config.KEY_USERNAME,username);
+                params.put(Config.KEY_PASSWORD,password);
+                params.put(Config.KEY_EMAIL, email);
+                return params;
+            }
+
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+}
